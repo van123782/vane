baca1e5ef9d14f3f527fe53922ae9ada7a83470e
van142415 <74314753+van142415@users.noreply.github.com>
2020-11-11 19:08:43 -0600

    Delete mi-footer.js

--- van123782.github.io/cmp/mi-footer.js
+++
@@ -1,7 +0,0 @@
-class MiFooter extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = /* html */
-      `Copyright &copy; Vazquez Cisneros Elda Vanessa`;
-  }
-}
-customElements.define("mi-footer", MiFooter);
